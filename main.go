ba75f384641bf5bf1b0e14aa5a9e913e6d4a1f96
jacremer <jacremer@live.com>
2021-04-27 00:30:14 -0700

    concurrent multi board testing
    need to configure waitgroup to prevent blocking after write block finishes

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"sudoku-solver/sudoku"
+	"time"
+)
+
+const (
+	size       int = 9
+	difficulty int = 1
+	tests      int = 10
 )
 
 func main() {
+	writer := make(chan string)
+	defer close(writer)
+
+	singleTest(size, difficulty)
+	fmt.Printf("\n")
+
+	go multiTest(writer)
+	for i := range writer {
+		fmt.Printf("%v\n", i)
+	}
+}
+
+func singleTest(size, difficulty int) {
 	board := sudoku.Create(9, 3)
 	board.Display()
 	board.Solve()
 	board.Display()
+}
+
+func multiTest(writer chan<- string) {
+	for i := 1; i <= tests; i++ {
+		go func(i int) {
+			board := sudoku.Create(size, difficulty)
+			start := time.Now().Nanosecond()
+			board.Solve()
+			elapsed := ((time.Now().Nanosecond() - start) / 1000000)
+			writer <- fmt.Sprintf("Board %d: %d steps, %dms", i, board.Steps, elapsed)
+		}(i)
+	}
 }
